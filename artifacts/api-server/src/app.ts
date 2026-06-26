a9352838300a935363bfbac0b2f39e3ff626fe95
hajdehamk-oss <hajdeha.mk@gmail.com>
2026-06-26 22:58:39 +0000

    ?!

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,5 +1,7 @@
-import express, { type Express } from "express";
+import express from "express";
+import type { Express } from "express";
 import cors from "cors";
+import type { IncomingMessage, ServerResponse } from "http";
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
@@ -10,14 +12,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: IncomingMessage & { id?: unknown }) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: ServerResponse) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,10 +27,10 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
 
 export default app;
