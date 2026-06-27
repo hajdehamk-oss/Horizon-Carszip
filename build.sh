4a050f9d4d2dcb2cd7e9d65ee161877076d7761a
hajdehamk-oss <hajdeha.mk@gmail.com>
2026-06-27 00:47:40 +0000

    push

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 set -e
 pnpm --filter @workspace/db exec tsc -p tsconfig.json
 pnpm --filter @workspace/api-zod exec tsc -p tsconfig.json
-pnpm --filter @workspace/api-server run typecheck
 pnpm --filter @workspace/api-client-react exec tsc -p tsconfig.json
 pnpm --filter @workspace/horizone run build
