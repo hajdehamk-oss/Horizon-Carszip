f8afe2158f8f7e1cb8444461c064010955efcf50
hajdehamk-oss <hajdeha.mk@gmail.com>
2026-06-27 00:12:56 +0000

    fix

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,32 +1,31 @@
 import express from "express";
 import cors from "cors";
 import type { IncomingMessage, ServerResponse } from "http";
-import pinoHttp from "pino-http";
+import pinoHttp, { type Options } from "pino-http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 
 const app = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req: IncomingMessage & { id?: unknown }) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: ServerResponse) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+const httpLogger = (pinoHttp as unknown as (opts: Options) => any)({
+  logger,
+  serializers: {
+    req(req: IncomingMessage & { id?: unknown }) {
+      return {
+        id: req.id,
+        method: req.method,
+        url: req.url?.split("?")[0],
+      };
     },
-  }),
-);
+    res(res: ServerResponse) {
+      return {
+        statusCode: res.statusCode,
+      };
+    },
+  },
+});
 
+app.use(httpLogger);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
